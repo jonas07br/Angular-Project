dcbaf82c4c3a2644010a9e3b13651d95cac778ad
jonas07br <jonasrafa07@gmail.com>
2024-09-20 14:19:04 -0300

    add random backgroung-color

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -1,19 +1,34 @@
-import { NgIf } from '@angular/common';
-import { Component } from '@angular/core';
+import { NgIf, NgStyle } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-big-card',
   standalone: true,
-  imports: [NgIf],
+  imports: [NgIf,RouterLink,NgStyle],
   templateUrl: './big-card.component.html',
   styleUrl: './big-card.component.css'
 })
 export class BigCardComponent {
-  projectName:string='projectName';
-  projectDescription:string="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. ";
-  projectType:string='projectType';
+  @Input() projectName:string='projectName';
+  @Input() projectDescription:string="projectDescription";
+  @Input() projectType:string='projectType';
+  @Input() projectId:string='projectId';
+  bgColor:string='blue';
   showDetails:boolean = true;
 
+  ngOnInit() {
+    this.bgColor = this.getRandomColor();
+  }
+
+  getRandomColor(): string {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
   show(){
     this.showDetails = !this.showDetails;
   }
