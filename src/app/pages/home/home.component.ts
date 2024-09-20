c651e7954b2546518eed9013151957da4b9ebf38
jonas07br <jonasrafa07@gmail.com>
2024-09-20 14:19:56 -0300

    add multiples cards

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,16 +1,21 @@
+import { projects } from './../../service/projects-service';
 import { Component, Input } from '@angular/core';
 import { TittlePageComponent } from "../../components/tittle-page/tittle-page.component";
 import { BigCardComponent } from "../../components/big-card/big-card.component";
 import { SmallCardComponent } from "../../components/small-card/small-card.component";
 import { NavBarComponent } from '../../components/nav-bar/nav-bar.component';
-
+import { NgFor } from '@angular/common';
+import * as projectsService from '../../service/projects-service';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [TittlePageComponent, BigCardComponent, SmallCardComponent,NavBarComponent],
+  imports: [TittlePageComponent, BigCardComponent, SmallCardComponent,NavBarComponent,NgFor],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
   theme: string='light';
+  projectsB = projects.slice(0,(projects.length)/2);
+  projectsS = projects.slice(projects.length/2,projects.length);
+
 }
