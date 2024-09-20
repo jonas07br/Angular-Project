2e7257a3de7290f31ba56a0370134c7b20d8bab8
jonas07br <jonasrafa07@gmail.com>
2024-09-20 14:19:42 -0300

    add random bg-color and dinamic tittles

--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { NgFor, NgStyle } from '@angular/common';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-small-card',
   standalone: true,
-  imports: [],
+  imports: [NgStyle],
   templateUrl: './small-card.component.html',
   styleUrl: './small-card.component.css'
 })
-export class SmallCardComponent {
-  projectName:string = 'projectName';
+export class SmallCardComponent implements OnInit {
+  @Input() projectName:string = 'projectName';
+  @Input() projectId:string = 'projectId';
+
+  bgColor:string = 'blue';
+  ngOnInit() {
+    this.bgColor = this.getRandomColor();
+  }
+
+  getRandomColor(): string {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
 }
